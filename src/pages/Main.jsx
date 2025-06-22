44e79898b25d1fda2b41abc5b508221eab032d49
XXXUDI <sashamlgg@gmail.com>
2025-06-22 13:02:55 +0200

    add Skeleton for use and banner

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,10 +3,12 @@ import styles from './styles.module.css';
 import NewsBanner from "../components/banner/banner.jsx";
 import {getNews} from "../api/apiNews.js";
 import NewsList from '../components/NewsList/NewsList.jsx';
+import Skeleton from '../components/Skeleton/Skeleton.jsx';
 
 const Main = () => {
 
     const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchNews = async () => {
@@ -15,20 +17,28 @@ const Main = () => {
                 setNews(response.news);
             } catch (err) {
                 console.log('Error fetching news: ', err);
+            } finally {
+                setIsLoading(false);
             }
         };
 
         fetchNews();
     }, []);
 
+    if(isLoading) {
+        console.log("Loading posts...");
+    }
 
     return (
         <main className={styles.main} >
-            {news.length > 0
+            {news.length > 0 && !isLoading
             ? <NewsBanner item={news[0]} />
-            : <div className={styles.loading}>Loading...</div>
+            : <Skeleton count={1} type='banner' />
             }
-            <NewsList news={news} />
+            {!isLoading 
+            ? <NewsList news={news}/>
+            : <Skeleton count={10} type='item'/>
+            }
         </main>
     );
 };
